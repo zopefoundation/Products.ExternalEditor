e6f7f5e845684b8ca2b93dc274c551bb9c56dba8
Tres Seaver <tseaver@palladion.com>
2001-05-28 20:56:10 +0000

    - Make sure that we update the catalog after editing metadata    (thanks to robert@rocad.ch for pointing this out).

--- CMFDefault/DublinCore.py
+++ CMFDefault/DublinCore.py
@@ -427,6 +427,7 @@ class DefaultDublinCoreImpl( PropertyManager ):
         self.setFormat( format )
         self.setLanguage( language )
         self.setRights( rights )
+        self.reindexObject()
 
     security.declareProtected( CMFCorePermissions.ModifyPortalContent
                              , 'manage_metadata' )
