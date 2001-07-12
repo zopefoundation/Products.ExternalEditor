95cb6252eb7354ce8ed1d4a31727d8e37bb43191
Jens Vagelpohl <jens@dataflake.org>
2001-07-12 15:51:46 +0000

    Applied patch from Chris Withers which avoids quoting problems for the error message if a problem occurs (PTK Tracker 339)

--- CMFDefault/skins/control/register.py
+++ CMFDefault/skins/control/register.py
@@ -9,13 +9,17 @@ portal_registration = context.portal_registration
 if not portal_properties.validate_email:
   failMessage = portal_registration.testPasswordValidity(password, confirm)
   if failMessage:
-      return REQUEST.RESPONSE.redirect(context.absolute_url() +'/join_form?error=%s' % (failMessage))
+      return context.join_form( context, REQUEST, error=failMessage )
+      
 failMessage = portal_registration.testPropertiesValidity(REQUEST)
+
 if failMessage:
-    return REQUEST.RESPONSE.redirect(context.absolute_url() +'/join_form?error=%s' % (failMessage))
+    return context.join_form( context, REQUEST, error=failMessage )
 else:
     password=REQUEST.get('password') or portal_registration.generatePassword()
     portal_registration.addMember(REQUEST['username'], password, properties=REQUEST)
+
     if portal_properties.validate_email or REQUEST.get('mail_me', 0):
         portal_registration.registeredNotify(REQUEST['username'])
-    return context.registered(context,REQUEST)
+
+    return context.registered( context, REQUEST )
