8417e5fdb83b86acb4273b6a7555878eea683080
Jens Vagelpohl <jens@dataflake.org>
2001-09-26 18:16:25 +0000

    When rejecting an item the reviewer will now be redirected back to the search page for pending content items.
    the redirect back to the content object itself blew up if the reviewer
    was just a reviewer but not a manager since plain reviewers are not allowed
    to view private content

--- CMFDefault/skins/content/content_status_modify.py
+++ CMFDefault/skins/content/content_status_modify.py
@@ -7,8 +7,12 @@ context.portal_workflow.doActionFor(
     workflow_action,
     comment=comment)
 
-context.REQUEST[ 'RESPONSE' ].redirect( '%s/view?%s'
-                   % ( context.absolute_url()
-                     , 'portal_status_message=Status+changed.'
-                     ) )
+if workflow_action == 'reject':
+    redirect_url = context.portal_url() + '/search?review_state=pending'
+else:
+    redirect_url = '%s/view?%s' % ( context.absolute_url()
+                                  , 'portal_status_message=Status+changed.'
+                                  )
+
+context.REQUEST[ 'RESPONSE' ].redirect( redirect_url )
 
