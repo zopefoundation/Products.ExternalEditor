8fcb0d2cd9dd5ecf1ba1243be660d8e4fa03d055
Tres Seaver <tseaver@palladion.com>
2001-10-07 00:29:55 +0000

    - Added "Change and View" submit button to content / folder editing    forms;  added check for this button to POST handlers in CMFDefault,    and indirected redirect targets in those methods through    'getActionByID'.

--- CMFDefault/skins/content/folder_edit.py
+++ CMFDefault/skins/content/folder_edit.py
@@ -1,12 +1,19 @@
 ## Script (Python) "folder_edit"
-##parameters=title, description
+##parameters=title, description, choice=' Change '
 ##title=Edit a folder
  
 context.edit( title=title,
               description=description)
 
-qst='?portal_status_message=Folder+changed.'
- 
-context.REQUEST.RESPONSE.redirect( context.absolute_url()
-                                 + '/folder_contents' + qst )
+qst='portal_status_message=Folder+changed.'
+
+if choice == ' Change and View ':
+    target_action = context.getTypeInfo().getActionById( 'view' )
+else:
+    target_action = context.getTypeInfo().getActionById( 'edit' )
+
+context.REQUEST.RESPONSE.redirect( '%s/%s?%s' % ( context.absolute_url()
+                                                , target_action
+                                                , qst
+                                                ) )
 
