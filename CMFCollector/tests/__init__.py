3ac706deec86c6c231549a817951edd6022cc812
Ken Manheimer <ken.manheimer@gmail.com>
2001-10-10 19:19:10 +0000

    Erase traces of CMFCalender (which was used as a rough template for the CMFCollector code).

--- CMFCollector/tests/__init__.py
+++ CMFCollector/tests/__init__.py
@@ -1,6 +1,5 @@
-"""\
-Unit test package for CMFCalendar.
+"""Unit test package for CMFCollector.
 
 As test suites are added, they should be added to the
-mega-test-suite in Products.CMFCalendar.tests.test_all.py
+mega-test-suite in Products.CMFCollector.tests.test_all.py
 """
