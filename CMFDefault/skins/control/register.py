1056a0e217eaa1611855a78a74f53114784448a8
Jens Vagelpohl <jens@dataflake.org>
2001-10-10 20:04:49 +0000

    workaround to make error message show up in the join form, esp. for ZPT

--- CMFDefault/skins/control/register.py
+++ CMFDefault/skins/control/register.py
@@ -9,11 +9,13 @@ portal_registration = context.portal_registration
 if not portal_properties.validate_email:
   failMessage = portal_registration.testPasswordValidity(password, confirm)
   if failMessage:
+      REQUEST.set( 'error', failMessage )
       return context.join_form( context, REQUEST, error=failMessage )
       
 failMessage = portal_registration.testPropertiesValidity(REQUEST)
 
 if failMessage:
+    REQUEST.set( 'error', failMessage )
     return context.join_form( context, REQUEST, error=failMessage )
 else:
     password=REQUEST.get('password') or portal_registration.generatePassword()
