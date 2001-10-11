dc3e211459939e63eff51d6598213928b9570d17
Ken Manheimer <ken.manheimer@gmail.com>
2001-10-11 23:55:15 +0000

    Dispatch with addition of assignees from collector_issue_followup_form.

--- CMFCollector/skins/collector/collector_issue_followup.py
+++ CMFCollector/skins/collector/collector_issue_followup.py
@@ -2,11 +2,9 @@
 ##parameters=comment, action
 ##title=Submit a new comment.
 
-context.portal_workflow.doActionFor(context,
-                                    action,
-                                    comment=comment)
-
-attachments = context.REQUEST.get('attachments', [])
-id = context.do_action(action, comment, attachments)
+context.do_action(action,
+                  comment,
+                  attachments=context.REQUEST.get('attachments', []),
+                  assignees=context.REQUEST.get('assignees', []))
 
 context.REQUEST.RESPONSE.redirect(context.absolute_url())
