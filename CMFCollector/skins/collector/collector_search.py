500d9bc199360c88ca780e1f93d7faf91e415d74
Ken Manheimer <ken.manheimer@gmail.com>
2001-10-16 22:38:57 +0000

    Provide for different index and (search) field names, and implement a few more fields.
    Still have to unravel the operations for the consolidated Subject
    attributes.

--- CMFCollector/skins/collector/collector_search.py
+++ CMFCollector/skins/collector/collector_search.py
@@ -2,35 +2,41 @@
 ##title=Build Collector Search
 
 query = {}
+query['sort_on'] = 'id'
 query['Type'] = "Collector Issue"
 query['path'] = context.absolute_url(1)
 
 reqget = context.REQUEST.get
 subj_items = []
 
-def supplement_query(field, reqget=reqget, query=query):
+def supplement_query(field, index_name=None, reqget=reqget, query=query):
+    if not index_name: index_name = field
     val = reqget(field, None)
     if val:
-        query[field] = val
-def supplement_subject_one(field, reqget=reqget, items=subj_items):
+        query[index_name] = val
+def supplement_subject_one(field, index_name=None, reqget=reqget, items=subj_items):
+    if not index_name: index_name = field
     val = reqget(field, None)
     if val:
-        items.append('%s:%s' % (field, val))
-def supplement_subject_many(field, reqget=reqget, items=subj_items):
+        items.append('%s:%s' % (index_name, val))
+def supplement_subject_many(field, index_name=None, reqget=reqget, items=subj_items):
+    if not index_name: index_name = field
     vals = reqget(field, [])
     for i in vals:
-        items.append('%s:%s' % (field, i))
+        items.append('%s:%s' % (index_name, i))
 
 supplement_query("SearchableText")
 supplement_query("Creator")
-supplement_subject_many("classifications")
-supplement_subject_many("severities")
+supplement_query("status", "review_state")
+supplement_subject_many("classifications", "classification")
+supplement_subject_many("severities", "severity")
+supplement_subject_many("supporters", "assigned_to")
 supplement_subject_one("resolution")
 supplement_subject_one("security_related")
 supplement_subject_one("reported_version")
-supplement_subject_one("security_related")
-supplement_subject_one("reported_version")
-if query:
+if subj_items:
     query["Subject"] = subj_items
 
 return context.portal_catalog(REQUEST=query)
+
+# Use "sort_on='index_name'" to sort - default, id
