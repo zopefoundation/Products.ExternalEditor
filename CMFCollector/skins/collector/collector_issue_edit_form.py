40b20fc60b625e1f59871384a537db1fb9b555f3
Ken Manheimer <ken.manheimer@gmail.com>
2001-10-18 20:09:03 +0000

    Replacing with template, since this will not be a trivial redirect.

--- CMFCollector/skins/collector/collector_issue_edit_form.py
+++
@@ -1,6 +0,0 @@
-## Script (Python) "collector_issue_edit_form.py"
-##parameters=
-##title=Redirect to issue contents with edit parameter
- 
-context.REQUEST.RESPONSE.redirect(context.absolute_url() + '?do_edit=1')
-
