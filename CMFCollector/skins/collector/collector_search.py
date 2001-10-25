178427b4275b2abbd2776254c532c8744df3a92f
Ken Manheimer <ken.manheimer@gmail.com>
2001-10-25 21:59:24 +0000

    Adjust to the internal catalog, getting rid of the complicated Subject index overloading scheme.
    Also, counting on the nicely normalized security_related settings.

--- CMFCollector/skins/collector/collector_search.py
+++ CMFCollector/skins/collector/collector_search.py
@@ -4,7 +4,6 @@
 query = {}
 query['sort_on'] = 'created'
 query['Type'] = "Collector Issue"
-query['path'] = "/".join(context.getPhysicalPath())
 
 reqget = context.REQUEST.get
 subj_items = []
@@ -14,47 +13,36 @@ def supplement_query(field, index_name=None, reqget=reqget, query=query):
     val = reqget(field, None)
     if val:
         query[index_name] = val
-def supplement_subject_one(field, index_name=None,
-                           reqget=reqget, items=subj_items):
-    if not index_name: index_name = field
-    val = reqget(field, None)
-    if val:
-        items.append('%s:%s' % (index_name, val))
-def supplement_subject_many(field, index_name=None,
-                            reqget=reqget, items=subj_items):
-    if not index_name: index_name = field
-    vals = reqget(field, [])
-    for i in vals:
-        items.append('%s:%s' % (index_name, i))
 
 supplement_query("SearchableText")
 supplement_query("Creator")
-supplement_subject_many("classifications", "classification")
-supplement_subject_many("severities", "severity")
-supplement_subject_many("supporters", "assigned_to")
-supplement_subject_one("resolution")
-supplement_subject_one("reported_version")
+supplement_query("classifications", "classification")
+supplement_query("severities", "severity")
+supplement_query("supporters", "assigned_to")
+supplement_query("resolution")
+supplement_query("reported_version")
 
 sr = reqget("security_related", [])
-
 if sr:
     if 'Yes' in sr and 'No' in sr:
+        # Both means we don't care - don't include in query.
         pass
     elif 'Yes' in sr:
-        subj_items.append('security_related:1')
+        query['security_related'] = [1]
     else:
-        subj_items.append('security_related:0')
-
-if subj_items:
-    query["Subject"] = subj_items
+        query['security_related'] = [0]
 
 rs = []
+
 for i in reqget("status", []):
     rs.append(i)
     # Include confidential alternatives to selected states.
+    # XXX To account for changes, we should obtain all the possible states,
+    #     and just do token processing according to their names.
     if i in ['Pending', 'Accepted']:
         rs.append("%s_confidential" % i)
 if rs:
-    query['review_state'] = rs
+    query['status'] = rs
 
-return context.portal_catalog(REQUEST=query)
+got = context.get_internal_catalog()(REQUEST=query)
+return got
