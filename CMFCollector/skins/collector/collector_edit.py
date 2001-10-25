e86d58ad42c0797527b65b7e726f7ee0cefdb58a
Ken Manheimer <ken.manheimer@gmail.com>
2001-10-25 21:55:16 +0000

    Service the "reinstate catalog option", and provide feedback in the message about it.

--- CMFCollector/skins/collector/collector_edit.py
+++ CMFCollector/skins/collector/collector_edit.py
@@ -2,6 +2,8 @@
 ##parameters=title, description, email, abbrev, supporters, topics, classifications, importances, severities, versions, other_versions_spiel
 ##title=Configure Collector
  
+from Products.PythonScripts.standard import url_quote_plus
+
 changed = context.edit(title=title,
                        description=description,
                        abbrev=abbrev,
@@ -14,8 +16,16 @@ changed = context.edit(title=title,
                        versions=versions,
                        other_versions_spiel=other_versions_spiel)
 
-qst='?portal_status_message=Configuration+%s.' % ((changed and "changed")
-                                                  or "unchanged")
+if changed:
+    changes = "Configuration changed"
+else:
+    changes = "No configuration changes"
+
+if context.REQUEST.get('recatalog', None):
+    context.reinstate_catalog()
+    changes = changes + ", reindexed"
+
+msg = '?portal_status_message=%s.' % url_quote_plus(changes)
  
-context.REQUEST.RESPONSE.redirect(context.absolute_url() + qst )
+context.REQUEST.RESPONSE.redirect(context.absolute_url() + msg)
 
