3b1ddeda2b5a8d3ca372cf37924236de40c9b8dc
Ken Manheimer <ken.manheimer@gmail.com>
2001-10-26 23:54:22 +0000

    Implementing assignable collector managers, and "dispatching" mode, where only the managers receive issue update notifications outside of the consultation phase (ie, when assigned supporters are consulting with the requester).  See http://new.zope.org/Members/klm/ColDev/26 for details.

--- CMFCollector/skins/collector/collector_edit.py
+++ CMFCollector/skins/collector/collector_edit.py
@@ -1,29 +1,34 @@
 ## Script (Python) "collector_edit.py"
-##parameters=title, description, email, abbrev, supporters, topics, classifications, importances, version_info_spiel
+##parameters=title, description, email, abbrev, managers, supporters, dispatching, topics, classifications, importances, version_info_spiel
 ##title=Configure Collector
  
 from Products.PythonScripts.standard import url_quote_plus
 
-changed = context.edit(title=title,
+changes = context.edit(title=title,
                        description=description,
                        abbrev=abbrev,
                        email=email,
+                       managers=managers,
                        supporters=supporters,
+                       dispatching=dispatching,
                        topics=topics,
                        classifications=classifications,
                        importances=importances,
                        version_info_spiel=version_info_spiel)
 
-if changed:
-    changes = "Configuration changed"
-else:
+if not changes:
     changes = "No configuration changes"
+else:
+    changes = "Changed: " + changes
 
 if context.REQUEST.get('recatalog', None):
     context.reinstate_catalog()
-    changes = changes + ", reindexed"
+    changes = changes + ", Reinstated catalog"
 
 msg = '?portal_status_message=%s.' % url_quote_plus(changes)
  
-context.REQUEST.RESPONSE.redirect(context.absolute_url() + msg)
+context.REQUEST.RESPONSE.redirect("%s/%s%s"
+                                  % (context.absolute_url(),
+                                     "collector_edit_form",
+                                     msg))
 
