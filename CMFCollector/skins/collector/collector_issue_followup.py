648991a01252328c0d77e12d17aaac7b4efbe2c0
Ken Manheimer <ken.manheimer@gmail.com>
2001-10-29 05:16:40 +0000

    When completing, just return to the collector default browse.  We might could qualify the search to issues submitted after the current, but setting b_start to after the current is **wrong** - b_start refers to the index in the batch, which generally is way different than the issue number.  For now, do the simplest thing until it's worth the trouble to implement the right thing.  (Jim and other heavy collector users *do* like being able to continue the browse with the issue after the completed one, so it may be worth the while at some point.)

--- CMFCollector/skins/collector/collector_issue_followup.py
+++ CMFCollector/skins/collector/collector_issue_followup.py
@@ -12,10 +12,7 @@ context.do_action(action,
                   filetype=(REQUEST.get('filetype', 'file')))
 
 if context.status() in ['Resolved', 'Rejected', 'Deferred']:
-    collector = context.aq_parent
-    destination = collector.absolute_url()
-    if len(collector) > int(context.id):
-        destination = destination + "?b_start:int=%s" % int(context.id)
+    destination = context.aq_parent.absolute_url()
 else:
     destination = context.absolute_url()
 
