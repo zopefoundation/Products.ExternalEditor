bbfb1d8d5ad12566ae8acb1f878a82b34737e599
Ken Manheimer <ken.manheimer@gmail.com>
2001-11-15 23:03:18 +0000

    Implemented tolerance for sendmail delivery problems, without completely hiding the problems.
    Splash any followup error notice in the destination web page, using
    portal_status_message.

--- CMFCollector/skins/collector/collector_issue_followup.py
+++ CMFCollector/skins/collector/collector_issue_followup.py
@@ -2,18 +2,23 @@
 ##parameters=comment, action="comment"
 ##title=Submit a new comment.
 
+from Products.PythonScripts.standard import url_quote_plus
+
 REQUEST = context.REQUEST
 
-context.do_action(action,
-                  comment,
-                  assignees=REQUEST.get('assignees', []),
-                  file=REQUEST.get('file'),
-                  fileid=REQUEST.get('fileid', ''),
-                  filetype=(REQUEST.get('filetype', 'file')))
+got = context.do_action(action,
+                        comment,
+                        assignees=REQUEST.get('assignees', []),
+                        file=REQUEST.get('file'),
+                        fileid=REQUEST.get('fileid', ''),
+                        filetype=(REQUEST.get('filetype', 'file')))
 
 if context.status() in ['Resolved', 'Rejected', 'Deferred']:
     destination = context.aq_parent.absolute_url()
 else:
     destination = context.absolute_url()
+
+if got:
+    destination += '?portal_status_message=' + url_quote_plus(got)
 
 context.REQUEST.RESPONSE.redirect(destination)
