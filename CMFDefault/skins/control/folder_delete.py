3f32b722ee48476b437ad352af335212467b6b98
Jens Vagelpohl <jens@dataflake.org>
2001-12-11 16:23:32 +0000

    - prettified - do not pass extra REQUEST to manage_delObjects

--- CMFDefault/skins/control/folder_delete.py
+++ CMFDefault/skins/control/folder_delete.py
@@ -2,8 +2,14 @@
 ##title=Delete objects from a folder
 ##parameters=
 REQUEST=context.REQUEST
-if REQUEST.has_key('ids'):
-  context.manage_delObjects(REQUEST['ids'], REQUEST)
-  return REQUEST.RESPONSE.redirect(context.absolute_url() + '/folder_contents?portal_status_message=Deleted.')
+ret_url = context.absolute_url() + '/folder_contents'
+
+if REQUEST.has_key( 'ids' ):
+  context.manage_delObjects( REQUEST['ids'] )
+  qs = '?portal_status_message=Deleted.'
+  
 else:
-  return REQUEST.RESPONSE.redirect(context.absolute_url() + '/folder_contents?portal_status_message=Please+select+one+or+more+items+first.')
+  qs = '?portal_status_message=Please+select+one+or+more+items+first.'  
+
+
+return REQUEST.RESPONSE.redirect( ret_url + qs )
