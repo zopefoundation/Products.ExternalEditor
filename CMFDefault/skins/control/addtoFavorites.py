db031cf3713dfd2a2b6368143869639f455585c9
Andrew Sawyers <andrew@sawdog.com>
2002-01-07 18:56:26 +0000

    *merge to head change for tracker #440.

--- CMFDefault/skins/control/addtoFavorites.py
+++ CMFDefault/skins/control/addtoFavorites.py
@@ -9,6 +9,6 @@ if not hasattr(homeFolder, 'Favorites'):
 targetFolder = getattr( homeFolder, 'Favorites' )
 new_id='fav_' + str(int( context.ZopeTime()))
 myPath=context.portal_url.getRelativeUrl(context)
-targetFolder.invokeFactory( 'Favorite', id=new_id, title=context.Title(), remote_url=myPath)
+targetFolder.invokeFactory( 'Favorite', id=new_id, title=context.TitleOrId(), remote_url=myPath)
 url = '%s/%s' % (context.absolute_url(), context.getTypeInfo().getActionById('view',''))
 return context.REQUEST.RESPONSE.redirect(url)
