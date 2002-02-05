3d06b4f18153c26f277730c99e24fec68cc537a7
Ken Manheimer <ken.manheimer@gmail.com>
2002-02-05 21:28:38 +0000

    After deleting a page, try to return to its (first) parent.

--- CMFWiki/skins/wiki/wikipage_deleterename_handler.py
+++ CMFWiki/skins/wiki/wikipage_deleterename_handler.py
@@ -11,8 +11,16 @@ if REQUEST.has_key('Delete'):
     if not REQUEST.get('confirm_delete'):
         raise ValueError, ("You must check the 'confirm delete'"
                            " box to commit the deletion.")
-    context.delete()
     url = REQUEST.get('URL2') + '/FrontPage'
+
+    # Try to get a parent url, so we can return there
+    folder = context.aq_parent
+    for p in context.parents:
+        if hasattr(folder, p):
+            url = folder[p].wiki_page_url()
+            break
+
+    context.delete()
     
 elif REQUEST.has_key('Rename'):
     url = context.rename(REQUEST.get('new_id'))
