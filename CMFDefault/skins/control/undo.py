8f98bc7a1478aae32348049ed8dfa5bbce9be081
Tres Seaver <tseaver@palladion.com>
2002-04-03 00:10:35 +0000

    - Merge fixes for Tracker #468 from branch.

--- CMFDefault/skins/control/undo.py
+++ CMFDefault/skins/control/undo.py
@@ -1,11 +1,7 @@
 ## Script (Python) "undo"
 ##title=Undo transactions
 ##parameters=transaction_info
-try:
-    context.portal_undo.undo(context, transaction_info)
-except:
-    return context.REQUEST.RESPONSE.redirect(
-        'folder_contents?portal_status_message=Transaction(s)+could+not+be+undone.' )
-    
+context.portal_undo.undo(context, transaction_info)
+
 return context.REQUEST.RESPONSE.redirect(
     'folder_contents?portal_status_message=Transaction(s)+undone' )
