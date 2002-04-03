7e5c828876b050ae553163819707c99c1a9f1bb6
Tres Seaver <tseaver@palladion.com>
2002-04-03 16:54:45 +0000

    - Clean out deprecated CMFDecor product prior to creating CMF 1.3     release branch.

--- CMFDecor/README.txt
+++
@@ -1,4 +0,0 @@
-CMFDecor README
-
-  This product allows creation of filesystem-based skin methods using
-  Zope Page Templates.
--- CMFDecor/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""\
-Unit test package for CMFDecor.
-
-As test suites are added, they should be added to the
-mega-test-suite in Products.CMFDecor.tests.test_all.py
-"""
