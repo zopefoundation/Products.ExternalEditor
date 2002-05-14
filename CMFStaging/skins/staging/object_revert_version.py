9abba44fd42e1f1dcd267f025c68f113ba0fd086
Shane Hathaway <shane@hathawaymix.org>
2002-05-14 18:01:09 +0000

    Fixed tool names.

--- CMFStaging/skins/staging/object_revert_version.py
+++ CMFStaging/skins/staging/object_revert_version.py
@@ -1,13 +1,13 @@
 ##parameters=version_id, REQUEST=None
 ##title=Revert to a specific revision of an object
 
-if context.LockTool.locked(context):
-    context.LockTool.unlock(context)
+if context.portal_lock.locked(context):
+    context.portal_lock.unlock(context)
 
-context.StagingTool.revertToVersion(context, version_id)
+context.portal_versions.revertToVersion(context, version_id)
 
 if REQUEST is not None:
-    msg = 'Reverted to revision %s.' % version_id
+    msg = 'Reverted+to+revision+%s.' % version_id
     view = 'object_version_control_form'
     REQUEST.RESPONSE.redirect(
         '%s/%s?portal_status_message=%s' % (
