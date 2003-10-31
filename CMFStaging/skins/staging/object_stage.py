6d554fc4130d19bd3262ce2b5c34829d0f184e81
Shane Hathaway <shane@hathawaymix.org>
2003-10-31 17:19:51 +0000

    Converted object_stage_form into a full-page view.

--- CMFStaging/skins/staging/object_stage.py
+++ CMFStaging/skins/staging/object_stage.py
@@ -1,6 +1,6 @@
-##parameters=stage_name, comments, REQUEST=None
+##parameters=stage_names, comments, REQUEST=None
 ##title=Stage an object
 
-context.portal_staging.updateStages2(context, [stage_name], comments)
+context.portal_staging.updateStages2(context, stage_names, comments)
 if REQUEST is not None:
     REQUEST.RESPONSE.redirect(context.absolute_url() + "/view")
