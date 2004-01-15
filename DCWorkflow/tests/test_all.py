5638d9d3f9fc6e92231a6e6a5580ca57bff841f5
Shane Hathaway <shane@hathawaymix.org>
2004-01-15 17:14:44 +0000

    Added tests of role-mapping management.

--- DCWorkflow/tests/test_all.py
+++ DCWorkflow/tests/test_all.py
@@ -1,3 +1,19 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors. All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+# 
+##############################################################################
+"""DCWorkflow tests.
+
+$Id$
+"""
 from unittest import main
 
 import Testing
@@ -12,7 +28,7 @@ from Products.CMFCore.tests.base.utils import build_test_suite
 
 def test_suite():
     return build_test_suite('Products.DCWorkflow.tests',[
-        'test_DCWorkflow',
+        'test_DCWorkflow', 'test_roles',
         ])
 
 if __name__ == '__main__':
