379ec817ac6c9507ebee537f8c12e0fb94c81c4f
Zachery Bir <zbir@urbanape.com>
2004-03-17 03:00:50 +0000

    provide for the current behavior when used with base_cms, and prepare for use with PluggableAuthService, defaulting, in the absence of either to an empty tuple.

--- DCWorkflow/Guard.py
+++ DCWorkflow/Guard.py
@@ -67,12 +67,18 @@ class Guard (Persistent, Explicit):
         if self.groups:
             # Require at least one of the specified groups.
             u = sm.getUser()
-            if hasattr(aq_base(u), 'getContextualGroupMonikers'):
-                u_groups = u.getContextualGroupMonikers(ob)
+            b = aq_base( u )
+            if hasattr( b, 'getContextualGroupMonikers' ):
+                u_groups = u.getContextualGroupMonikers( ob )
+            elif hasattr( b, 'getGroupsInContext' ):
+                u_groups = u.getGroupsInContext( ob )
+            elif hasattr( b, 'getGroups' ):
+                u_groups = u.getGroups()
             else:
                 u_groups = ()
             for group in self.groups:
-                if ('(Group) %s' % group) in u_groups:
+                if ( group in u_groups
+                  or '(Group) %s' % group in u_groups ):
                     break
             else:
                 return 0
