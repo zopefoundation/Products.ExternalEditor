761b77532fc2d399e932541e4819d13b9e07db0b
Zachery Bir <zbir@urbanape.com>
2004-04-28 19:58:53 +0000

    Added ZPL to all modules

--- plugins/DelegatingMultiPlugin.py
+++ plugins/DelegatingMultiPlugin.py
@@ -1,9 +1,20 @@
 ##############################################################################
 #
-# DelegatingMultiPlugin   Shim to use any User Folder with the
-#                         PluggableAuthenticationService
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights
+# Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this
+# distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
 #
 ##############################################################################
+""" DelegatingMultiPlugin   Shim to use any User Folder with the
+                            PluggableAuthenticationService
+"""
 
 __doc__     = """ Delegating User Folder shim module """
 __version__ = '$Revision$'[11:-2]
--- plugins/SearchPrincipalsPlugin.py
+++ plugins/SearchPrincipalsPlugin.py
@@ -1,10 +1,21 @@
 ##############################################################################
 #
-# SearchPrincipalsPlugin   Plugin to delegate enumerateUsers
-#                          and enumerateGroups requests to another 
-#                          PluggableAuthService
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights
+# Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this
+# distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
 #
 ##############################################################################
+""" SearchPrincipalsPlugin   Plugin to delegate enumerateUsers
+                             and enumerateGroups requests to another 
+                             PluggableAuthService
+"""
 
 # General Python imports
 import copy, os
