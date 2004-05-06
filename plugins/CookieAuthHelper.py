aac5e03af20a6fccdc35e3cd50ac5998683c3503
Tres Seaver <tseaver@palladion.com>
2004-05-06 19:41:03 +0000

    -Fix isomorph of Zope bug #1226.

--- plugins/CookieAuthHelper.py
+++ plugins/CookieAuthHelper.py
@@ -120,6 +120,7 @@ class CookieAuthHelper(Folder, BasePlugin):
                 request.set('__ac_password', '')
 
                 cookie_val = encodestring('%s:%s' % (login, password))
+                cookie_val = cookie_val.replace( '\n', '' )
                 response = request['RESPONSE']
                 response.setCookie(self.cookie_name, cookie_val, path='/')
 
