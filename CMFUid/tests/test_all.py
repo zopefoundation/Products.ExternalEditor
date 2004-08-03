10dd5ecdb725abe25b64a7a67b6315e832f1215b
Gregoire Weber <gregweb@incept.ch>
2004-08-03 20:09:11 +0000

    - added test_all.py used by all_cmf_tests.py

---
+++ CMFUid/tests/test_all.py
@@ -0,0 +1,38 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" CMFCore tests.
+
+$Id$
+"""
+
+from unittest import main
+import Testing
+import Zope
+Zope.startup()
+
+from Products.CMFCore.tests.base.utils import build_test_suite
+
+
+def suite():
+    return build_test_suite('Products.CMFUid.tests',[
+        'test_uidannotation',
+        'test_uidgeneration',
+        'test_uidhandling',
+        ])
+
+def test_suite():
+    # Just to silence the top-level test.py
+    return None
+
+if __name__ == '__main__':
+    main(defaultTest='suite')
