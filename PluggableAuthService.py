bacf86f47c785be5b599d49fe3fcafd9ef897c56
Lennart Regebro <regebro@gmail.com>
2004-08-31 09:27:45 +0000

    Implemented the challenge call, so challende plugins now gets called. Only raise "Redirect", url supported as challenge, but anything else requires a rewrite of BasicRequest...

--- PluggableAuthService.py
+++ PluggableAuthService.py
@@ -250,7 +250,7 @@ class PluggableAuthService( Folder ):
         , name
         , value
         ) = self._getObjectContext( request[ 'PUBLISHED' ], request )
-
+        
         for user_id, login in user_ids:
 
             user = self._findUser( plugins, user_id, login
@@ -275,8 +275,24 @@ class PluggableAuthService( Folder ):
                 return user
 
         if not is_top:
-            return None
-
+            # Try to validate with user folders higher up.
+            innerob=self.aq_parent
+            while hasattr(innerob,"aq_parent"):
+                inner = getattr(innerob, 'aq_inner', innerob)
+                parent = getattr(inner, 'aq_parent', None)
+                if parent is not None:
+                    innerob = parent
+                else:
+                    if hasattr(innerob, 'im_self'):
+                        innerob = innerob.im_self
+                        innerob = getattr(innerob, 'aq_inner', innerob)
+                if hasattr(innerob,"__allow_groups__"):
+                    userfolder=innerob.__allow_groups__
+                    if hasattr(userfolder,"validate"):
+                        user=userfolder.validate(request,auth,roles)
+                        if user is not None:
+                            return user
+                        
         #
         #   No other user folder above us can satisfy, and we have no user;
         #   return a constructed anonymous only if anonymous is authorized.
@@ -291,6 +307,18 @@ class PluggableAuthService( Folder ):
                               ):
             return anonymous
 
+        # No validation in upper user folders: Make a challenge
+        challengers = plugins.listPlugins(IChallengePlugin)
+        for challenger_id, challenger in challengers:
+            try:
+                # A successful challenge involves raising a 
+                # "Redirect", url exception.
+                challenger.challenge(request, request.RESPONSE)
+            except _SWALLOWABLE_PLUGIN_EXCEPTIONS:
+                LOG('PluggableAuthService', WARNING, 
+                    'ChallengePlugin %s error' % challenger_id,
+                    error=sys.exc_info())
+        
         return None
 
     security.declareProtected( SearchPrincipals, 'searchUsers')
@@ -723,7 +751,6 @@ class PluggableAuthService( Folder ):
 
             if user_id is not None:
                 result.append( ( user_id, name ) )
-
         return result
 
     security.declarePrivate( '_unmangleId' )
@@ -733,7 +760,7 @@ class PluggableAuthService( Folder ):
 
     security.declarePrivate( '_mangleId' )
     def _mangleId( self, namespace, id ):
-
+    
         return MANGLE_DELIMITER.join( ( namespace, id ) )
 
     security.declarePrivate( '_computeMangledId' )
