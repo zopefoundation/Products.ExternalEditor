58580170c718064a7140047eb9cf9d10430d3e02
Jens Vagelpohl <jens@dataflake.org>
2005-03-24 20:05:23 +0000

    - Forward-port fixes that were inexplicably added on a single branch only

--- slurp_release.py
+++ slurp_release.py
@@ -7,6 +7,7 @@ import os
 import httplib
 import getopt
 import base64
+import mimetypes
 
 CVSROOT = ':pserver:anonymous@cvs.zope.org:/cvs-repository'
 
@@ -176,10 +177,16 @@ options:
         """
         URL = ( '/Products/CMF/%s/%s' % ( self._version_id, filename ) )
         body = open( filename ).read()
+        content_type, content_enc = mimetypes.guess_type(URL)
+        headers = self._getAuthHeaders()
+        headers['Content-Length'] = len(body)
+        headers['Content-Type'] = content_type
+        headers['Content-Encoding'] = content_enc
 
         conn = httplib.HTTPConnection( 'www.zope.org' )
         print 'PUTting file, %s, to URL, %s' % ( filename, URL )
-        conn.request( 'PUT', URL, body, self._getAuthHeaders() )
+
+        conn.request( 'PUT', URL, body, headers )
         response = conn.getresponse()
         if int( response.status ) not in ( 200, 201, 204, 302 ):
             raise ValueError, 'Failed: %s (%s)' % ( response.status
