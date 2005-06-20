2e6fda622da25c79d149c8147860f03198b65f88
Chris McDonough <chrism@plope.com>
2005-06-20 23:17:09 +0000

    More prep for 0.9 release.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import sys
 from distutils.core import setup
+from zopeedit import __version__
+
 try:
     import py2exe
 except ImportError:
@@ -9,8 +11,15 @@ except ImportError:
 else:
     packages = ['Plugins']
 
-setup(name='zopeedit', 
+setup(name='zopeedit',
+      version=__version__,
+      description="Zope External Editor",
+      author="Casey Duncan and Contributors, maintained by Chris McDonough",
+      author_email="chrism@plope.com",
+      url="http://www.plope.com/software/ExternalEditor",
       scripts=['zopeedit.py'],
       windows=['zopeedit.py'],
       options={"py2exe": {"packages": ["encodings", "Plugins", "win32com"]}},
-      packages=packages)
+      
+      packages=packages
+      )
