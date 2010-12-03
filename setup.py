b0faf8741b63ec4751b69d971ad69d9d95118b90
Matthias Broquet <mbroquet@atreal.net>
2010-12-03 11:00:48 +0000

    oOps, I forgot to delete the 'dev' of the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='Products.ExternalEditor',
-      version='1.1.0dev',
+      version='1.1.0',
       description="Zope External Editor",
       long_description=open("README.txt").read() + "\n" + \
                        open("CHANGES.txt").read(),
