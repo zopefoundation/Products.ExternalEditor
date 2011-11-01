f8effbc48d1e3733b561c143a35a3d63c7b4798b
Laurence Rowe <l@lrowe.co.uk>
2011-11-01 23:00:00 +0000

    Update dtml to Zope trunk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='Products.ExternalEditor',
-      version='1.1.0dev',
+      version='2.0.0dev',
       description="Zope External Editor",
       long_description=open("README.txt").read() + "\n" + \
                        open("CHANGES.txt").read(),
